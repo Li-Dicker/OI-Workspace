078bcc15a81cbff910683e14993cc8a786ed13bc
X-X <60730214+Li-Dicker@users.noreply.github.com>
2020-10-19 07:31:56 +0800

    第一次提交

---
+++ .history/tmpelate_20200908185925.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define int long long
+#define N 11111
+#define INF 0x3f3f3f3f3f
+using namespace std;
+
+namespace IOstream
+{
+	#define print(a,b) printf("%lld",a),putchar(b)
+	
+	int input()
+	{
+		int INPUT;
+		scanf("%lld",&INPUT);
+		return INPUT;
+	}
+}
+using namespace IOstream;
+
+signed main()
+{
+	
+	return 0;
+}
